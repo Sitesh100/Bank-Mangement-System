77bad067e973f2ddb1b59cd3a7080b650d1e35a5
Sketch <siteshkjha9@gmail.com>
2024-11-21 02:20:03 +0530

    Add:project is completed

--- src/bank/management/system/login.java
+++ src/bank/management/system/login.java
@@ -1,5 +1,6 @@
 package bank.management.system;
 
+import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -99,13 +100,24 @@ public class login extends JFrame implements ActionListener {
         try{
             if (e.getSource()==button1){
 
-
+                Connn c = new Connn();
+                String cardno = textField2.getText();
+                String pin = passwordField3.getText();
+                String q = "select * from login where card_number = '"+cardno+"' and  pin = '"+pin+"'";
+                ResultSet resultSet = c.statement.executeQuery(q);
+                if (resultSet.next()){
+                    setVisible(false);
+                    new main_Class(pin);
+                }else {
+                    JOptionPane.showMessageDialog(null,"Incorrect Card Number or PIN");
+                }
 
             }else if (e.getSource() == button2){
                 textField2.setText("");
                 passwordField3.setText("");
             }else if (e.getSource() == button3){
-
+                new Signup();
+                setVisible(false);
             }
         }catch (Exception E){
             E.printStackTrace();
